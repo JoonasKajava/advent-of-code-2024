829f449d7c388a018a9ebe363dd3f03dc7e243eb
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2024-12-03 14:19:12 +0200

    feat: day 3 part 1

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -1,3 +1,41 @@
+use std::fs;
+
+use regex::Regex;
+
+fn find_muls(string: &str) -> Vec<(usize, usize)> {
+    let regex = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+    let mut results = vec![];
+
+    for (_, [first, second]) in regex.captures_iter(string).map(|c| c.extract()) {
+        results.push((
+            first.parse::<usize>().unwrap(),
+            second.parse::<usize>().unwrap(),
+        ));
+    }
+    results
+}
+
+fn add_up(values: Vec<(usize, usize)>) -> usize {
+    values.iter().fold(0usize, |acc, e| acc + (e.0 * e.1))
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("./src/input.txt").unwrap();
+    let muls = find_muls(&input);
+    let sum = add_up(muls);
+    dbg!(sum);
+}
+
+#[test]
+fn test_find_muls() {
+    let result =
+        find_muls("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))");
+
+    assert_eq!(result, vec![(2, 4), (5, 5), (11, 8), (8, 5)])
+}
+
+#[test]
+fn test_add_up() {
+    let values = vec![(2, 4), (5, 5), (11, 8), (8, 5)];
+    assert_eq!(add_up(values), 161);
 }
